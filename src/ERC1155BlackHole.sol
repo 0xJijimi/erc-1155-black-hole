b62fc16086675fe4459bb228d8f90dbe3d4b1f1a
0xjijimi <0xjijimi@gmail.com>
2024-09-10 13:18:39 +0200

    TokensReceived event and handlers

--- src/ERC1155BlackHole.sol
+++ src/ERC1155BlackHole.sol
@@ -4,10 +4,40 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
 contract ERC1155BlackHole is ERC1155Holder {
-    // This contract is immutable, so it doesn't need a constructor or any state variables
+    // Event emitted when tokens are received
+    event TokensReceived(
+        address indexed operator,
+        address indexed from,
+        uint256[] ids,
+        uint256[] values,
+        bytes data
+    );
 
-    // Prevent non-ERC1155 tokens to be sent to the contract
-    receive() external payable {
-        revert("This contract only accepts ERC-1155 tokens");
+    // Function to handle batch receipt of ERC1155 tokens
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] memory ids,
+        uint256[] memory values,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        emit TokensReceived(operator, from, ids, values, data);
+        return super.onERC1155BatchReceived(operator, from, ids, values, data);
+    }
+
+    // Function to handle single ERC1155 token receipt
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        uint256[] memory ids = new uint256[](1);
+        uint256[] memory values = new uint256[](1);
+        ids[0] = id;
+        values[0] = value;
+        emit TokensReceived(operator, from, ids, values, data);
+        return super.onERC1155Received(operator, from, id, value, data);
     }
 }
